49eef409b6f6d569100cff0de678d0b072f39481
Ahmedbash98 <W0777010@myscc.ca>
2022-05-23 21:37:46 -0400

    added content to the content-card.component

---
+++ A_Abdella_MyFavouriteFootballPlayer/src/app/models/list.ts
@@ -0,0 +1,20 @@
+import { Content } from "./content";
+
+export class List {
+
+ static contentCount = 0;
+  private _items: Content[];
+  constructor(item: Content) {
+    this._items = [];
+    // this._items[0] = item;
+    this.addCount();
+  }
+
+  get items():Content[] {
+    return this._items;
+  }
+  addCount() {
+    return ++List.contentCount;
+  }
+
+}
